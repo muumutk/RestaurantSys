135ca257f7a536109da2a16c3934a3e21e4ebe5b
mumu <anuccyan@gmail.com>
2025-08-24 22:36:54 +0800

    0824 #addSupplier

---
+++ RestaurantSys/Controllers/SuppliersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RestaurantSys.Access.Data;
+using RestaurantSys.Models;
+
+namespace RestaurantSys.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly RestaurantSysContext _context;
+
+        public SuppliersController(RestaurantSysContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Suppliers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Supplier.ToListAsync());
+        }
+
+        // GET: Suppliers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Supplier
+                .FirstOrDefaultAsync(m => m.SupplierID == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Suppliers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SupplierID,SupplierName,ContactPerson,SupplierTel,Address")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(supplier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Supplier.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SupplierID,SupplierName,ContactPerson,SupplierTel,Address")] Supplier supplier)
+        {
+            if (id != supplier.SupplierID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supplier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupplierExists(supplier.SupplierID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        private bool SupplierExists(int id)
+        {
+            return _context.Supplier.Any(e => e.SupplierID == id);
+        }
+    }
+}
