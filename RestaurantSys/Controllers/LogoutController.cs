65405122553522e9fbef91ed5dd396ec160a0f75
mumu <anuccyan@gmail.com>
2025-09-15 23:14:32 +0800

    0915 #cartItemClear

--- RestaurantSys/Controllers/LogoutController.cs
+++ RestaurantSys/Controllers/LogoutController.cs
@@ -12,8 +12,8 @@ namespace RestaurantSys.Controllers
             // 清除所有的登入 Cookie
             await HttpContext.SignOutAsync("MemberLogin");
 
-            // 登出後導向首頁或菜單頁
-            return RedirectToAction("Index", "Menu");
+            // 登出後導向一個專門用來清除前端資料的 View
+            return View("LogoutAndClear");
         }
     }
 }
